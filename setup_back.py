2fe050b3c6c41cfb3004f560aecac119e863adb5
guillaumemouchet <guillaume.mouchet01@outlook.com>
2023-05-11 16:39:54 +0200

    setup back and front test final?

--- setup_back.py
+++ setup_back.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="EzPwd Back",
-    version="0.1.0", # TODO Change manually
+    version="0.1.1", # TODO Change manually
     description="password address with flask",
     long_description=open("README.md").read(),
     author="benjamin.mouchet, guillaume.mouchet",
     package_dir={"EzPwd_Back": "./Projet/logger"},
-    install_requires=open("Projet/requirements.txt").readlines(),
+    install_requires=open("Projet/logger/requirements.txt").readlines(),
 )
 
 
--- setup_front.py
+++ setup_front.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="EzPwd Front",
-    version="0.1.0", # TODO Change manually
+    version="0.1.1", # TODO Change manually
     description="password address with flask",
     long_description=open("README.md").read(),
     author="benjamin.mouchet, guillaume.mouchet",
