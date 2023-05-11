f05839295a52e50674bb8323b70f1f0717d33a34
guillaumemouchet <guillaume.mouchet01@outlook.com>
2023-05-11 18:12:47 +0200

    changed setup path

--- Projet/logger/setup_back.py
+++ Projet/logger/setup_back.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="EzPwd Back",
-    version="0.1.1", # TODO Change manually
+    version="0.1.2", # TODO Change manually
     description="password address with flask",
     long_description=open("README.md").read(),
     author="benjamin.mouchet, guillaume.mouchet",
-    package_dir={"EzPwd_Back": "./Projet/logger"},
+    package_dir={"EzPwd_Back": "."},
     install_requires=open("Projet/logger/requirements.txt").readlines(),
 )
 
